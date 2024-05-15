30d2a9e4c3897acc285e0fcf911a2f753cd838a3
ritoolya3355 <ritoolya@gmail.com>
2024-05-15 19:31:41 +0300

    HW1

---
+++ HW01_js/main.js
@@ -0,0 +1,9 @@
+let userAgeValue = 45;
+let userNameNickname = 'Margo'
+console.log(userNameNickname);
+console.log(userAgeValue);
+
+userNameNickname = 'Valerie';
+userAgeValue = 22;
+console.log(userNameNickname);
+console.log(userAgeValue);
